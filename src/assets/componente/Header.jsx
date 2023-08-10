b674ac157ebbe972f0ba3eed675aad865bdc9e88
DaniG1menez <dani.gimenez5330@gmail.com>
2023-08-09 21:29:18 -0300

    dasdas

---
+++ src/assets/componente/Header.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import MenuEnlaces from './MenuEnlaces'
+import './Header.css'
+import '../../EstilosGenerales.css'
+
+const Enca = () => {
+    return (
+
+    <div className="header">
+   
+    <div>
+        Portal de eventos
+    </div>
+    <MenuEnlaces/>
+    </div>
+    
+    )
+} 
+
+export default Enca
---
+++ src/assets/componente/login.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function login() {
+  return (
+        <div className="flex">
+            <div className="material-icons-outlined">login</div>
+       </div>
+  )
+}
+
+export default login
